7d76f1f0dcc42b82caf23cdb95f271e0c48c8654
HP <HP@DESKTOP-QH6JTDM>
2021-06-29 20:44:11 +0530

    Commit all files

---
+++ spring-pivotal-cloud-foundary-demo-with-gradle/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	
+	@GetMapping("/")
+	public String getMessage()
+	{
+		return "Welcome to Docker Demo";
+	}
+}
